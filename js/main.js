e31aa8cbbcf527942d09437ef48d76afcbcb9951
CrustyMarsh <56603629+CrustyMarsh@users.noreply.github.com>
2020-06-02 15:22:56 +0100

    teste do json

--- js/main.js
+++ js/main.js
@@ -1,15 +1,32 @@
+$(document).ready(function () {
+    e = new Produto("f", "f", "f");
+    console.log(e.quantidade);
 
-//catálogo? cesto?
+    e.decrementar();
+    console.log(e.quantidade);
 
+    e.incrementar();
+    console.log(e.quantidade);
 
-e = new Produto("f", "f", "f");
-console.log(e.quantidade);
+    e.quantidade = 4;
+    console.log(e.quantidade);
 
-e.decrementar();
-console.log(e.quantidade);
+    $.getJSON("../data/produtos.json", function (json) {
+        console.log(json); // this will show the info it in firebug console
+    });
+});
 
-e.incrementar();
-console.log(e.quantidade);
-
-e.quantidade = 4;
-console.log(e.quantidade);
+function ajaxHelper(uri, method, data) {
+    self.error(''); // Clear error message
+    return $.ajax({
+        type: method,
+        url: uri,
+        dataType: 'json',
+        contentType: 'application/json',
+        data: data ? JSON.stringify(data) : null,
+        error: function (jqXHR, textStatus, errorThrown) {
+            console.log("AJAX Call[" + uri + "] Fail...");
+            self.error(errorThrown);
+        }
+    });
+}
