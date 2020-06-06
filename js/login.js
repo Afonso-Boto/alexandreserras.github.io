93ba72bcb39ff113382d839bbbc5e1f5064c397e
Afonso <afonso.boto@ua.pt>
2020-06-06 17:45:54 +0100

    Login Page

---
+++ js/login.js
@@ -0,0 +1,102 @@
+
+(function ($) {
+    "use strict";
+
+    
+    /*==================================================================
+    [ Validate ]*/
+    var input = $('.validate-input .input100');
+
+    var objLogin = [
+        {
+            username: "AfonsoBoto",
+            password: "afonso"
+        },
+        {
+            username: "TomasCandeias",
+            password: "tomas"
+        },
+        {
+            username: "AlexandreSerras",
+            password: "alexandre"
+        },
+        {
+            username: "DiogoMonteiro",
+            password: "diogo"
+        },
+    ]
+
+    $('.validate-form').on('submit',function(){
+        var check = true;
+
+        if (checkCredentials(input) == false)
+            check = false;
+        console.log(check);
+      
+
+
+        
+        for(var i=0; i<input.length; i++) {
+            if(validate(input[i]) == false){
+                showValidate(input[i]);
+                check=false;
+            }
+        }
+
+        
+        
+        return check;
+    });
+
+
+    function checkCredentials(input) {
+        var user = document.getElementById("username").value;
+        var pass = document.getElementById("password").value;
+
+        for (var i = 0; i < objLogin.length; i++) {
+            if (user == objLogin[i].username && pass == objLogin[i].password) {
+                console.log(username + " is logged IN!");
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    $('.validate-form .input100').each(function(){
+        $(this).focus(function(){
+           hideValidate(this);
+        });
+    });
+
+
+    function validate(input) {
+       
+
+        if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+                return false;
+            }
+        }
+        else {
+            if($(input).val().trim() == ''){
+                return false;
+            }
+        }
+    }
+
+    function showValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).addClass('alert-validate');
+    }
+
+    function hideValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).removeClass('alert-validate');
+    }
+    
+    
+
+})(jQuery);
