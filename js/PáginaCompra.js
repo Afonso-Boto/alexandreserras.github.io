b97ff05c7aa3baf1d686e9a0901bfb708ca7112d
Tomás Candeias <tomaslcandeias@outlook.com>
2020-06-08 22:10:28 +0000

    pequenas alterações
    ola123

--- js/PáginaCompra.js
+++ js/PáginaCompra.js
@@ -1,31 +1,34 @@
 ﻿$(document).ready(function () {
-    // viewModel - contém os data-binds realizados na página
-    var viewModel = {
-        displayList: ko.observableArray(),
-        tagList: ko.observableArray(),
-        displayName: ko.observable("CityFarm"),
-        searchTerm: ko.observable(""),
-        removeProduto: function (produto) {
-            viewModel.displayList.remove(produto);
-            localStorage.setItem("car", JSON.stringify(viewModel.displayList()))
-        },
-        precoTotal: ko.pureComputed(function () {
-            var t = 0;
-            viewModel.displayList().forEach(function (p) {
-                t += p._preco * p._quantidade;
-            });
-            return t
-        })
-    }
-    viewModel.displayList(JSON.parse(localStorage.getItem("car")));
-    $("#finaliza").on("click", function () {
-        viewModel.displayList.removeAll();
-        localStorage.setItem("car", "[]")
-    })
+	// viewModel - contém os data-binds realizados na página
+	var viewModel = {
+		displayList: ko.observableArray(),
+		tagList: ko.observableArray(),
+		displayName: ko.observable("CityFarm"),
+		searchTerm: ko.observable(""),
+		removeProduto: function (produto) {
+			viewModel.displayList.remove(produto);
+			localStorage.setItem("car", JSON.stringify(viewModel.displayList()))
+		},
+		precoTotal: ko.pureComputed(function () {
+			var t = 0;
+			viewModel.displayList().forEach(function (p) {
+				t += p._preco * p._quantidade;
+			});
+			t = Math.round(t * 100) / 100;
+			return t
+		})
+	}
+
+
+	viewModel.displayList(JSON.parse(localStorage.getItem("car")));
+	$("#finaliza").on("click", function () {
+		viewModel.displayList.removeAll();
+		localStorage.setItem("car", "[]")
+	})
    
    $("#logout").click( function(){
 	   localStorage.setItem("logged", false);
    })
-    
-    ko.applyBindings(viewModel);
+	
+	ko.applyBindings(viewModel);
 });
