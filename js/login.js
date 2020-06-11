8aa2cb526aba40fc68acefe0fc81d97e4ff3bb03
Tomás Candeias <tomaslcandeias@outlook.com>
2020-06-11 16:32:25 +0000

    tentativa de login 1

--- js/login.js
+++ js/login.js
@@ -1,102 +1,102 @@
 
 (function ($) {
-    "use strict";
-    
+	"use strict";
+	
 
-    
-    /*==================================================================
-    [ Validate ]*/
-    var input = $('.validate-input .input100');
+	
+	/*==================================================================
+	[ Validate ]*/
+	var input = $('.validate-input .input100');
 
-    var objLogin = [
-        {
-            username: "afonsoboto@ua.pt",
-            password: "afonso"
-        },
-        {
-            username: "tomascandeias@ua.pt",
-            password: "tomas"
-        },
-        {
-            username: "alexandreserras@ua.pt",
-            password: "alexandre"
-        },
-        {
-            username: "diogomonteiro@ua.pt",
-            password: "diogo"
-        },
-    ]
+	var objLogin = [
+		{
+			username: "afonsoboto@ua.pt",
+			password: "afonso"
+		},
+		{
+			username: "tomascandeias@ua.pt",
+			password: "tomas"
+		},
+		{
+			username: "alexandreserras@ua.pt",
+			password: "alexandre"
+		},
+		{
+			username: "diogomonteiro@ua.pt",
+			password: "diogo"
+		},
+	]
 
-    $('.validate-form').on('submit',function() {
-        var check = true;
+	$('.validate-form').on('submit',function() {
+		var check = true;
 
-        if (checkCredentials() == false)
-            check = false;
-        console.log(check);
-      
-
-
-        
-        for(var i=0; i<input.length; i++) {
-            if(validate(input[i]) == false){
-                showValidate(input[i]);
-                check=false;
-            }
-        }
-
-        
-        return check;
-    });
+		if (checkCredentials() == false)
+			check = false;
+		console.log(check);
+	  
 
 
-    function checkCredentials() {
-        var user = $("#username").val();
-        var pass = $("#password").val();
+		
+		for(var i=0; i<input.length; i++) {
+			if(validate(input[i]) == false){
+				showValidate(input[i]);
+				check=false;
+			}
+		}
 
-        for (var i = 0; i < objLogin.length; i++) {
-            if (user == objLogin[i].username && pass == objLogin[i].password) {
-                localStorage.setItem("logged", true);
-                return true;
-            }
-        }
-        localStorage.setItem("logged", false);
-        return false;
-    }
-
-    $('.validate-form .input100').each(function(){
-        $(this).focus(function(){
-           hideValidate(this);
-        });
-    });
+		
+		return check;
+	});
 
 
-    function validate(input) {
-       
+	function checkCredentials() {
+		var user = $("#username").val();
+		var pass = $("#password").val();
 
-        if ($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
-            if ($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null ) {
-                return false;
-            }
-        }
-        else {
-            if ($(input).val().trim() == '' || checkCredentials() == false){
-                return false;
-            }
-        }
-    }
+		for (var i = 0; i < objLogin.length; i++) {
+			if (user == objLogin[i].username && pass == objLogin[i].password) {
+				localStorage.setItem("logged", true);
+				return true;
+			}
+		}
+		localStorage.setItem("logged", false);
+		return false;
+	}
 
-    function showValidate(input) {
-        var thisAlert = $(input).parent();
+	$('.validate-form .input100').each(function(){
+		$(this).focus(function(){
+		   hideValidate(this);
+		});
+	});
 
-        $(thisAlert).addClass('alert-validate');
-    }
 
-    function hideValidate(input) {
-        var thisAlert = $(input).parent();
+	function validate(input) {
+	   
 
-        $(thisAlert).removeClass('alert-validate');
-    }
-    
-    
+		if ($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+			if ($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null ) {
+				return false;
+			}
+		}
+		else {
+			if ($(input).val().trim() == '' || checkCredentials() == false){
+				return false;
+			}
+		}
+	}
+
+	function showValidate(input) {
+		var thisAlert = $(input).parent();
+
+		$(thisAlert).addClass('alert-validate');
+	}
+
+	function hideValidate(input) {
+		var thisAlert = $(input).parent();
+
+		$(thisAlert).removeClass('alert-validate');
+	}
+	
+	
 
 })(jQuery);
